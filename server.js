0dbab1e05cf48971d72594db5e4d53e3d337e631
Sartik0709 <sharmasartik@gmail.com>
2024-06-10 20:53:49 +0530

    eval4

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.DB_URL;
 mongoose.connect(MONGO_URI).then(() => {
     log('info', 'MongoDB connected successfully.');
